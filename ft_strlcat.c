54bca02bcd1af0bd6756ac4f1e040d3990829e64
EvVillca <evertvillca@gmail.com>
2024-10-25 10:28:19 +0200

    libft

---
+++ ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evillca- <evillca-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 16:29:05 by evillca-          #+#    #+#             */
+/*   Updated: 2024/10/22 16:29:05 by evillca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ * return = dest_len + src_len
+*/
+size_t 	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dest_len;
+	size_t	src_len;
+
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = dest_len;
+	j = 0;
+	//if (size == 0)
+	//	return (src_len);
+	if (size <= dest_len)
+		return (src_len + size);
+	while (src[j] && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
